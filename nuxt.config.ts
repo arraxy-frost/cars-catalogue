cd245e5acd2d696ab8159ce8d89fa4deef0b281e
arraxy-frost <artyom.prilip@gmail.com>
2025-08-31 19:32:20 +0600

    chore: update favicon to new car icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2025-07-15",
+    ssr: true,                          // Enable Server Side Rendering
+    devtools: { enabled: true },
+    css: [
+        '@/assets/global.css'
+    ],
+    app: {
+        head: {
+            title: "Cars Catalogue",
+            link: [
+                { rel: 'icon', type: 'image/png', href: '/icon-car.png' },
+            ]
+        }
+    }
+});
